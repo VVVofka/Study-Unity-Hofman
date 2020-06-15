89e7f7196521006d1a480f63d5a6a0cf4afa291c
Vladimir Bogdanov <vvvofka@yandex.ru>
2020-06-15 15:55:27 +0300

    to home

---
+++ Assets/Scripts/TrainControll/Param.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Luses {
+    left = 1,
+    center = 2,
+    right = 3
+}
+
+public class Param{
+    public Luses luse = Luses.left;        // № лузы
+    public float alfa = 0.0f;   // угол в градусах входа в лузу
+    public float a = 1.0f;      // расстояние между прицельным и лузой в радиусах
+    public float b = 5.0f;      // расстояние между битком и лузой
+    public float d = 0.0f;      // резка
+    public float dwrong = 0.0f; // резка неправильная
+    List<Color32> history = new List<Color32>();
+    int left = 3;
+    float rnd = 1.0f;
+    float krnd = 0.71f;
+
+    public Param(Luses Luse, float Alfa, float A, float B, float D, float DWrong) {
+        luse = Luse;
+        alfa = Alfa;
+        a = A;
+        d = D;
+        b = B;
+    } // ///////////////////////////////////////////////////////////////////////////
+    public bool setGood() {
+        history.Add(new Color32(0, 255, 0, 255));
+        rnd *= krnd;
+        return --left <= 0;
+    } // ///////////////////////////////////////////////////////////////////////////////////////////////////
+    public bool setWrong() {
+        history.Add(new Color32(255, 0, 0, 255));
+        rnd /= krnd;
+        return --left <= 0;
+    } // ///////////////////////////////////////////////////////////////////////////////////////////////////
+    public float getRnd() {
+        return Random.Range(0.0f, rnd);
+    } // //////////////////////////////////////////////////////////////////////////////////////////////////
+} // ********************************************************************************************************
---
+++ Assets/Scripts/TrainControll/Params.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using UnityEngine;
+
+public class Params : MonoBehaviour {
+    List<Param> lstAll = new List<Param>();
+    List<Param> lstInWork = new List<Param>();
+    Luses[] luses = {Luses.left, Luses.center, Luses.right};
+    float[] valfa = { -10, -5, 5, 10};
+    float[] va = {0.5f, 1.0f, 1.5f};
+    float[] vb = {5f, 6f, 7.5f};
+    float[] vd = {1f / 3f, 1f / 4f};
+
+    void Start() {
+        int n = 0;
+        foreach(Luses l in luses)
+            foreach(float alfa in valfa)
+                foreach(float a in va)
+                    foreach(float b in vb) {
+                        lstAll.Add(new Param(Luses.left, alfa, a, b, vd[0], vd[1]));
+                        lstInWork.Add(lstAll[n++]);
+                        for(int nd = 1; nd < vd.Length - 1; nd++) {
+                            lstAll.Add(new Param(Luses.left, alfa, a, b, vd[nd], vd[nd - 1]));
+                            lstInWork.Add(lstAll[n++]);
+                            lstAll.Add(new Param(Luses.left, alfa, a, b, vd[nd], vd[nd + 1]));
+                            lstInWork.Add(lstAll[n++]);
+                        }
+                        lstAll.Add(new Param(Luses.left, alfa, a, b, vd[vd.Length - 1], vd[vd.Length - 2]));
+                        lstInWork.Add(lstAll[n++]);
+                    }
+    }
+} // /////////////////////////////////////////////////////////////////////////
+void Update() { }
+} // ******************************************************************************
