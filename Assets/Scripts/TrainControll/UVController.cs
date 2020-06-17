22deef207bd8075eb361fed0a80baa3d1ee1d48b
Vladimir Bogdanov <vvvofka@yandex.ru>
2020-06-17 12:08:47 +0300

    to policlinic

---
+++ Assets/Scripts/TrainControll/UVController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;   // Импортируем фреймворк для работы с кодом UI.
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+    [SerializeField] private Text scoreLabel;   // Объект сцены Reference Text для задания свойства text.
+
+    void Update() {
+        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+    } // ////////////////////////////////////////////////////////////////////////////////////
+    public void OnOpenSettings() {  //  Метод, вызываемый кнопкой настроек.
+        Debug.Log("open settings");
+    } // ///////////////////////////////////////////////////////////////////////////////////////
+} // ***************************************************************************************
